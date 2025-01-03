e6e6c0d08558a631cb7553d86381e5c60f598e9e
Ajay <ajayjangir95211@gmail.com>
2025-01-03 16:18:06 +0530

    new file "TextArea"

---
+++ test.js
@@ -0,0 +1,9 @@
+// const date1 = new Date("2024-12-26T10:22:10.645538+00:00");
+// const date2 = new Date("2024-11-26T10:22:10");
+// console.log(date2 - date1);
+
+// const booking = { name: "John", total_guests: 3 };
+
+// console.log();
+
+console.log(new Date().toISOString().split("T")[0]);
